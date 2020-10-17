d1173a43d6795ee6b59720f02c62a57f3743ae0d
Liam Horne <liam@lihorne.com>
2020-10-17 09:10:28 -0400

    introduce Pipfile and repo cleanup

--- bin/sync.sh
+++ bin/sync.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-jupyter nbconvert --to notebook --inplace --execute ../apple_health_incremental_processor.ipynb
-jupyter nbconvert --to notebook --inplace --execute ../myfitnesspal-from-api.ipynb
+pipenv run jupyter nbconvert --to notebook --inplace --execute ../apple_health_incremental_processor.ipynb
+pipenv run jupyter nbconvert --to notebook --inplace --execute ../myfitnesspal-from-api.ipynb
+pipenv run nbstripout ../*.ipynb
